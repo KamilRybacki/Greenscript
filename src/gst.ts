02c492525a9f945b4966aa3f819a94b57a038457
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-06-17 16:17:02 +0200

    removed unused imports

--- src/gst.ts
+++ src/gst.ts
@@ -1,7 +1,3 @@
-import * as Compile from './translate/compile';
-import * as Parse from './translate/parse';
-import * as Utils from './translate/utils';
-
 const gst = () => {
   return (classesTemplateLiteral: TemplateStringsArray) => {
     console.log(classesTemplateLiteral.raw[0]);

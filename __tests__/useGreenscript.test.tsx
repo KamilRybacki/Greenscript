9cfec5dee91739367b8b7c62b436fec4eb63fc81
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-06-27 17:23:53 +0200

    typos

--- __tests__/useGreenscript.test.tsx
+++ __tests__/useGreenscript.test.tsx
@@ -1,10 +1,8 @@
-/* eslint-disable require-jsdoc */
-
-import useGSAP from '../src/hooks/useGreenscript';
+import useGreenscript from '../src/hooks/useGreenscript';
 
 import * as Mocks from '../__mocks__/useGreenscriptMocks';
 
 test('Test useGreenscript output', () => {
-  const gsapInterface = useGSAP(Mocks.hookTestScript);
-  console.log(gsapInterface.stess);
+  const greenscriptInterface = useGreenscript(Mocks.hookTestScript);
+  console.log(greenscriptInterface);
 });
